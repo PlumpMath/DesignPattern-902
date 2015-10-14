7411cdbce0d9bac90ef38215f0ed8dbe172357df
owenhuang91 <owenhuang91@gmail.com>
2015-10-14 21:52:42 +0800

    update

---
+++ CommandPattern/CommandPattern/RemoteControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class RemoteControl
+    {
+        Command[] onCommands;
+        Command[] offCommands;
+
+        public RemoteControl()
+        {
+            onCommands = new Command[7];
+            offCommands = new Command[7];
+            Command noCommand = new NoCommand();
+
+            for (int i = 0; i < 7; i++)
+            {
+                onCommands[i] = noCommand;
+                offCommands[i] = noCommand;
+            }
+        }
+
+        public void setCommand(int slot,Command onCommand,Command offCommand)
+        {
+            onCommands[slot] = onCommand;
+            offCommands[slot] = offCommand;
+        }
+
+        public void onButtonWasPushed(int slot)
+        {
+            onCommands[slot].execute();
+
+        }
+
+        public void offButtonWasPushed(int slot)
+        {
+            offCommands[slot].execute();
+
+        }
+    }
+}
